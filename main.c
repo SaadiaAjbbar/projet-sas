862354c26becb98c37a314f42197f5279a507af5
SaadiaAjbbar <you@gmail.com>
2025-09-09 18:45:24 +0100

    declaration initial des varibles

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+typedef struct{
+    int id;
+    char nom[50];
+    int numeroMaillot;
+    char poste[50];
+    int age;
+    int buts;
+}Joueur;
+Joueur Equipe[100];
+int main() {
+    
+    
+    
+    return 0;
+}
